fb2483e17a64be9da36f4fe54c1ed8a6364760b9
fderliqu <florian.derlique@polytech-lille.net>
2022-03-13 19:25:53 +0100

    Ajout commentaire et modification mineurs de nombre magiques

--- etape2/libs/Log/strategy/libDNSinFILE.c
+++ etape2/libs/Log/strategy/libDNSinFILE.c
@@ -12,22 +12,24 @@
 FILE * file;
 
 int initLog(void * f){
-	file = fopen ((char *)f,"w");
-	if(file==NULL)
+	file = fopen ((char *)f,"w"); //Ouverture du fichier passé en paramètre en lecture
+	if(file==NULL)	// Si impossible à ouvrir le fichier
 	{
 		printf("Le fichier n'a pas pu être ouvert.\n");
-		exit(-1);
+		return(-1);
 	}
 	return 0;
 }
 
 int addLog(logMsg_t * paquet){
-	unsigned char* octet = (unsigned char *)paquet->msg;
+	unsigned char* octet = (unsigned char *)paquet->msg; //Stockage du premier element du message dans un pointeur pour parcourir le message
 	for(int i=0;i<paquet->size;i++){
-		fprintf(file,"%02x ",*octet);
-		octet++;
+		fprintf(file,"%02x ",*octet); //On print le code hexa
+		octet++; //On passe à l'octet suivant
 	}
 	fprintf(file,"\n");
 	fflush(file);
 	return 0;
 }
+
+
--- etape2/libs/Log/strategy/libPrintDomainName.c
+++ etape2/libs/Log/strategy/libPrintDomainName.c
@@ -9,32 +9,38 @@
 #include<fcntl.h>
 #include"../log_dns.h"
 
-/*int initLog(void * f){
+#define GoTo_BYTE2 2
+#define GoTo_QNAME 11
+#define MASK_OPCODE 0x78 //0b01111000 masque pour tester l'opcode
+
+int initLog(void * f){
+	//Cette fonction est inutile pour cette stratégie mais il faut quand même la définir, cette fonction ne fera rien, le code sert juste à eviter le warning dans la compilation
+	if(f==NULL)return 0;
 	return 0;
 }
-*/
+
 
 int addLog(logMsg_t * paquet){
 	unsigned char* octet = (unsigned char *)paquet->msg;
-	octet += 2; //On veut l'octet comprenant l'opcode
+	octet += GoTo_BYTE2; //On veut l'octet comprenant l'opcode
 	#ifdef DEBUG
 	printf("opcode : %x\n", *octet);
 	#endif
 
 	#ifdef DEBUG
-	printf("test : %x\n", (*octet & 0x78));
+	printf("test opcode : %x\n", (*octet & MASK_OPCODE));
 	#endif
-	if ((*octet & 0x78) == 0x00)  //on teste si opcode == 0
+	if ((*octet & MASK_OPCODE) == 0x00)  //on teste si opcode == 0
 	{
-		octet += 11; //on pointe sur le premier octet donnant le nom de domaine du QNAME
+		octet += GoTo_QNAME; //on pointe sur le premier octet donnant le nom de domaine du QNAME
 		#ifdef DEBUG
 		printf("first element in QNAME :%x\n", *octet);
 		#endif
-		while (*octet != 0x00)
+		while (*octet != 0x00) //RFC1035 : Le QNAME se termine par le caractère NUL de code ascii 0
 		{
 			printf("%c", *octet);
-			if (*octet < 32) printf("."); //si code ascii système on a remarqué que c'était en réalité un point
-			octet ++;
+			if (*octet < 32) printf("."); //si code ascii est une commande de controle de terminal on a remarqué que c'était en réalité un point, pour afficher le point, nous affichons un /'./' au lieu du caractère non imprimable
+			octet++; //On passe l'octet suivant
 		}
 		printf("\n");
 	}
